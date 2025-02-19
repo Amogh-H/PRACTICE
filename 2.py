f0c1c9d1ee06fbd69ff424696d7f4536ffb654ca
Amogh V Hiremath <amogh.hiremath@globallogic.com>
2025-02-19 16:30:31 +0530

    UPDATED

--- 2.py
+++ 2.py
@@ -4,6 +4,15 @@ def print_pyramid(height):
         print(" " * (height - i), end="")
         # Print stars
         print("*" * (2 * i - 1))
+    print("UPDATED ONE //////////////////")
+    print("UPDATED ONE //////////////////")
+    print("UPDATED ONE //////////////////")
+    print("UPDATED ONE //////////////////")
+    print("UPDATED ONE //////////////////")
+
+
+        
+
 
 # Example usage
 height = 5  # You can change this value to adjust the size of the pyramid

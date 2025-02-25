8574b110daf08903849acb53957ef47f65666ef5
Amogh <amogh.hiremath@globallogic.com>
2025-02-25 16:02:31 +0530

    updated CODE with new lines

--- hello.py
+++ hello.py
@@ -3,5 +3,13 @@ import pyjokes
 # Get a random joke
 joke = pyjokes.get_joke()
 
+
 # Print the joke
+# Print the joke
+# Print the joke
+
 print(joke)
+
+print("ldjf/////")
+print("ldjf/////")
+print("ldjf/////")

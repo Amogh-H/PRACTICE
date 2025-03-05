f3e6dd04bece5dabc1cad68c3bd33984decb8355
Amogh-H <amogh.hiremath@globallogic.com>
2025-03-05 15:38:56 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,7 +1,6 @@
-
-def add(a,b):
-  return a+b
+def add(a, b):
+    return a + b
 
 def test_add():
-  assert add(1,2) == 3
-  assert add(1. 0) == 1
+    assert add(1, 2) == 3
+    assert add(1, 0) == 1  # Corrected 
